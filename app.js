55afc59eb689cc8a54f3bea582eb8186bb5928a1
Shubham Vishwakarma <vi453770@gmail.com>
2025-10-27 16:46:24 +0530

    use import export module

--- app.js
+++ app.js
@@ -81,14 +81,14 @@
 
 // Spread Operator
 
-const arr1 = [1, 2];
-const arr2 = [...arr1, 3, 4];
-console.log(arr2);
+// const arr1 = [1, 2];
+// const arr2 = [...arr1, 3, 4];
+// console.log(arr2);
 
 
 // Rest Operator
 
-function RestFunc(...number) {
+export default function RestFunc(...number) {
     return number.reduce((acc, n) => acc + n, 0);
 }
-console.log(RestFunc(5, 8, 6));
+// console.log(RestFunc(5, 8, 6));
---
+++ check.js
@@ -0,0 +1,2 @@
+import RestFunc from "./app.js";
+console.log(RestFunc(7, 8, 2))
