9c17441f75d19eb0a3d2135120c54af45f873806
Shubham Vishwakarma <vi453770@gmail.com>
2025-10-27 16:41:52 +0530

    rest and spread operator

--- app.js
+++ app.js
@@ -55,25 +55,40 @@
 
 // fetch data
 
-const fetchData = async(req, res) => {
-    try {
-        // const response = await fetch('https://designer.mocky.io/', {
-        //     method: 'GET',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     }
-        // })
+// const fetchData = async(req, res) => {
+//     try {
+// const response = await fetch('https://designer.mocky.io/', {
+//     method: 'GET',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// })
 
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-        // console.log(response)
-        if (!response.ok) {
-            throw new Error('New Error Status Code : ', response.status);
-        }
-        const data = await response.json();
-        console.log(data.body)
-    } catch (error) {
-        console.error("catch block error : ", error.message)
-    }
+//         const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+//         // console.log(response)
+//         if (!response.ok) {
+//             throw new Error('New Error Status Code : ', response.status);
+//         }
+//         const data = await response.json();
+//         console.log(data.body)
+//     } catch (error) {
+//         console.error("catch block error : ", error.message)
+//     }
+// }
+
+// fetchData();
+
+
+// Spread Operator
+
+const arr1 = [1, 2];
+const arr2 = [...arr1, 3, 4];
+console.log(arr2);
+
+
+// Rest Operator
+
+function RestFunc(...number) {
+    return number.reduce((acc, n) => acc + n, 0);
 }
-
-fetchData();
+console.log(RestFunc(5, 8, 6));
